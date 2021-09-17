6dff844247bc00f95046b3dfe48d574f62dcff40
Mamata290 <mamata.moger@gmail.com>
2021-09-17 16:31:52 -0400

    ShareCode in Github

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products : [
+        {
+            _id:1,
+            name:'Nike Slim Shirts',
+            category:'Shirts',
+            image:'./images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:2,
+            name:`Men's Casual Shirt`,
+            category:'Shirts',
+            image:'./images/p2.jpg',
+            price:769,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:3,
+            name:'Fit Cotton Casual Shirt',
+            category:'Shirts',
+            image:'./images/p3.jpg',
+            price:699,
+            brand:'IndoPrimo',
+            rating:4.3,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:4,
+            name:'Formal Shirt',
+            category:'Shirts',
+            image:'./images/p4.jpg',
+            price:599,
+            brand:'JAI TEXTILES',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:5,
+            name:'Regular Casual Shirt',
+            category:'Shirts',
+            image:'./images/p5.jpg',
+            price:349,
+            brand:'Symbol',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:6,
+            name:'Relaxed Shirt',
+            category:'Shirts',
+            image:'./images/p6.jpg',
+            price:756,
+            brand:'spykar',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality product'
+        }
+
+    ]
+}
+export default data
